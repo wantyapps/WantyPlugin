8b59768435c97cb7023f5560ea00c582b09d1865
Uri Arev <uri.arev@gmail.com>
2021-02-28 12:26:26 +0200

    Trying to implement feat: Command Not Found

--- lua/plugin/plugin.lua
+++ lua/plugin/plugin.lua
@@ -3,10 +3,16 @@ local P = {}
 local function version()
 	print('1.0')
 end
-	
+
+local function commandNotFound()
+	print("Command not found.")
+end
+
 function P.command(cmd)
 	if cmd == "version" then
 		version()
+	else
+		commandNotFound()
 	end
 end
 
